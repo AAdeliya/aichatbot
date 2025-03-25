8aac2bbb2394ac8a1f6cad29345af36a2d6f694f
firstPush <adel.sdet2@gmail.com>
2025-03-24 21:47:22 -0500

    1-2 week again

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Plus_Jakarta_Sans } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import "./globals.css";
+
+const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "MailGenie",
+  description: "AI-powered sales assistant.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={jakarta.className}>
+          {/* <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider> */}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
