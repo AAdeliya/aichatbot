4f024959a66d68c48f25f8904656de44ecde0e23
firstPush <adel.sdet2@gmail.com>
2025-04-02 14:29:24 -0500

    Real-time Chat Backend with Server Actions & Hooks - in progress

---
+++ src/context/use-chat-context.ts
@@ -0,0 +1,5 @@
+// TODO: Create a context to store chatRoom, chats[], loading, and realtime toggle
+// TODO: Add ChatProvider to wrap the entire app or chat component tree
+// TODO: Add useChatContext hook for accessing context values
+
+// NOTE: This lets other hooks easily access and update chat state.
